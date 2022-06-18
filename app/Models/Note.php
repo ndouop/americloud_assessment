5db1af46c7c91676fb6c62c0908a077543aa54e8
Choutvet Roger <ncr.roger@gmail.com>
2022-06-18 14:45:31 +0100

    Create a Note Model

---
+++ app/Models/Note.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Note extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'resource_type',
+        'resource_id',
+        'content',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'resource_type' => 'string',
+        'resource_id' => 'string',
+        'content' => 'string',
+    ];
+}
